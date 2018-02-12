c167d95c9629ae586dc899347a49f302c0c0604c
Ana <avaquera@u.rochester.edu>
2018-02-12 01:02:56 -0500

    update

--- Group_Assignment2/index.php
+++ Group_Assignment2/index.php
@@ -1,58 +1,47 @@
-<?php include "inc/html-top.inc"; ?>
+<?php include "inc/html-top.inc";?>
+	<body class="home">
+		<div class= "container">
 
-	<body class="meghan">
+		<?php include "inc/header.inc";?>
+		<?php include "inc/nav.inc";?>
 
-	<div class= "container">
-	<?php include "inc/header.inc";?>
-	<?php include "inc/nav.inc"; ?>
+		<h1> Initial Roles </h1>
+
+		<div class="slider">
+        	<img src="images/abroad.jpg" alt="Meghan Patrick: study abraod">
+			<img src="images/favphoto.jpg" alt="Amber Williams: favorite photo">
+			<img src="images/toronto.jpg" alt="Ana Vaquera: Toronto, Canada">
+        </div>
 		
-		<h1> Meghan Patrick</h1>
-		
-		<ul> 
-			<li>Major: Mechanical Engineering, Minor: Computer Science </li>
-			<li>Year: Senior</li>
-			<li>Hometown: Freetown, MA </li>
-		</ul>
-
-			<h1>Work Experience</h1>
-			<h2>Harmonic</h2>
-				<p>
-							For my first internship, I worked on cable modem termination systems at Harmonic Inc. I developed and implemented engineering command line interface using Python for cable modem debug commands, in an effort to make back-end programming for efficient. I also integrated several different cable modem communication sources into one source to improve performance and usability. I utilized C++ to analyze and improve the system error logging protocol. Since error logs were challenging to sort through, I created a broader sorting mechanism to process errors in an effort to ease debug and error management. 	</p>
-			<h2>Raytheon</h2>
-				<p>
-						For my first internship, I worked on cable modem termination systems at Harmonic Inc. I developed and implemented engineering command line interface using Python for cable modem debug commands, in an effort to make back-end programming for efficient. I also integrated several different cable modem communication sources into one source to improve performance and usability. I utilized C++ to analyze and improve the system error logging protocol. Since error logs were challenging to sort through, I created a broader sorting mechanism to process errors in an effort to ease debug and error management. 	
-				</p>
-			<h2>Fluids Lab Research Assistant</h2>
-				<p>
-						This past summer I worked as a research assistant for the Fluid Dynamics Mixing Lab at the University of Rochester, led by Professor Kelley. Kelley’s lab consisted of twelve members, with representation from seven different countries. While his lab group supported several different topics of study from reactive mixing to liquid metal casting, I worked in the team centered around developing liquid metal batteries.</p>
-				<p>
-						TThe sun sets and winds die down, but homes always need power. This is why battery storage is so important for renewable energy sources, which have a severe disadvantage to fossil fuels that are consistently available. While a typical battery stores energy by separating electrons into two solid cells, a new energy storage solution is liquid metal batteries that have an entirely liquid core. These batteries are much more efficient at storing and dissipating energy, but are prone to failure due to uncontrolled fluid flow. This summer, I worked as a research assistant in the Fluid Dynamics Lab at the University of Rochester analyzing battery flows in an effort to improve the performance of these batteries. The central issue I studied is how to analyze flow patterns effectively. Ultrasound sensors can be used to study the velocities of opaque liquids, but only give a one dimensional view. To solve this issue, I developed an optimized configuration using multiple probes to visualize the 3D space of the battery. To do this, I used a computational model I developed in MATLAB of common flow patterns, and designed a testing fixture using PTC Creo CAD software to validate this model with physical experimentation. I then conducted experiments using signal processing software to show that my new probe configuration captured roughly four times as much information as traditional methods. The new technique I developed is now employed by the lab as an improved experimental apparatus to study the flows which often lead liquid metal batteries to fail. Through the continued study of this issue, better energy storage can be developed giving renewable energy the same consistency as fossil fuels. </p>
-		
-
-			<h1> Study Abroad </h1>
-			<p>
-				I spent the spring semester of my junior year in Madrid, Spain. I never studied Spanish in school, so choosing my abroad program and diving right into this foreign language and culture was quite the adventure. I spent all of winter break leading up to my departure studying and practicing Spanish, with multiple language books, Rosetta Stone, and Duolingo. I was anxious and nervous and sleep deprived the first day, when the taxi driver dropped me off at my host family’s apartment and I knocked on the door to meet the people I would be living with for the next four months. I had rehearsed how to greet and respond to my host parents a hundred times, but when my host Dad first greeted me I was so completely overwhelmed and confused. I couldn’t understand a single word he said and me and my host mom sat in the living room that day for hours in near silence, bonding over a mutual love of their hyper dog, Hara. 
-			</p>
-
-
-			<figure class="images">
-					<img src="images/abroad.jpg" alt="Portugal Pic" >
-					<figcaption>Sunset in Lisbon, Portugal</figcaption>
+		<div class="meg">
+		<h2> Meghan Patrick: Information Architect </h2>
+			<figure>
+				<img src="images/favphoto.jpg" alt="favphoto">
+				<figcaption>My favorite photo.</figcaption>
 			</figure>
+			<p> My initital role for this assignment is infromaiton architect. As information architect I must prepare all the content and how it's arranged within each page and across pages. Design the navigation and how it's presented in any browser type and size. Lead team collaboration (Github) and manage the file structure – unless there is a Backend Hacker on the team. I will be ensure that there are no usability problems. That the structure layer (content) is coded in HTML so it is arranged and accessible in a way that makes sense for the content. That content structure(HTML)meets industry standards and best practices including W3C HTML validation within reason and that the team is coordinated and effective.</p>
+		</div>
 
-			<p>
-				As the days went on, I became much more acquainted with the language and culture and made many friends through the IES program. I took four class: Introductory Spanish, Spanish Art & Architecture, Controls Engineering, and Artificial Intelligence. Each week my language skills grew exponentially, and soon I was able to comfortably converse with my host parents over long meals.
-			</p>
-			<p>
-				I explored the depths of Madrid, visiting every park and art museum. I explored the nightlife and exciting boroughs of the city, walking up to 15 miles each day just wandering the streets. My engineering courses were taken on a spanish college campus, so I had the chance to meet spanish students and learn about their experience in college.
-			</p>
-			<p>
-				I traveled to many cities and towns throughout Spain including Barcelona, Toledo, Bilbao, San Sebastian, Sevilla, Valencia, Cordoba, Granada, Salamanca, Alcala de Henares, Burgos, and Vitoria-Gasteiz. I wanted to spend as much time as possible in Spain to fully absorb the language and culture, and learn as much as I could about the country. But, I also traveled to visit various friends who were also studying abroad and went on several trips with friends to other countries. I traveled to Portugal, Denmark, Ireland, the Netherlands, and Morocco. Overall it was an incredible journey and a dream come true. 
-			</p>
+ 		<div class="ber">
+		<h2> Amber Willaims: Design Artist </h2>
+			<figure class="images">
+				<img src="images/abroad.jpg" alt="Portugal Pic" >
+				<figcaption>Sunset in Lisbon, Portugal</figcaption>
+			</figure>
+			<p>My initital role for this assignment is design artist. As the artist I am responsible for everything the user sees, in any browser type and size I decide the graphic design, colors, styles, fonts, layouts for any browser type and size. I will ensure that there are noreadability problems. That the presentation layer (styles) is coded in CSS so it is visually arranged a way that makes sense for the content and adds value to the structure layer. Also that styles (CSS) meet industry standards and best practices including W3C CSS validation within reason.</p>
+		</div>
 
-<?php include "inc/footer.inc"; ?>
-	</div> <!--.container -->
+		<div class="an">
+		<h2> Ana Vaquera: Coder </h2>
+			<figure>
+				<img src="images/toronto.jpg" alt="Toronto, Canada">
+				<figcaption>Toronto, Canada</figcaption>
+			</figure>
+			<p>My initital role for this assignment is coder. As coder I will be responsible for everything the user interacts with, from page load to every click and scroll. Checks to make sure everything in the front-end (not just JavaScript but also HTML and CSS) must work and work well (optimized). Images must be correctly prepped (optimized). Decides the particulars of plug-in usage and installation, web font installation, and general optimization and usage of all front- end technologies. I will also be in charge of all server interactions using PHP and MySQL including the addition of all code in the HTML documents to handle the server-side interactions (server- side includes too). Responsible for the set up and manipulation of database(s). Leads team collaboration (Github) and manages the file structure.</p>
+        </div>
 
-	<?php include "inc/scripts.inc"; ?>
+		<?php include "inc/footer.inc"; ?>
+		</div> <!--.container -->
+		<?php include "inc/scripts.inc"; ?>
 	</body>
 </html>
