6297ca3ccbffd402c5bb70c8adb24c1a206ecba8
Ana <avaquera@u.rochester.edu>
2018-02-12 03:56:18 -0500

    update

--- Group_Assignment2/index.php
+++ Group_Assignment2/index.php
@@ -22,16 +22,7 @@
 				<figcaption>Sunset in Lisbon, Portugal</figcaption>
 			</figure>
 			
-			<p> My initital role for this assignment is infromaiton architect. As information architect I must prepare all the content and how it's arranged within each page and across pages. Design the navigation and how it's presented in any browser type and size. Lead team collaboration (Github) and manage the file structure – unless there is a Backend Hacker on the team. I will be ensure that there are no usability problems. That the structure layer (content) is coded in HTML so it is arranged and accessible in a way that makes sense for the content. That content structure(HTML)meets industry standards and best practices including W3C HTML validation within reason and that the team is coordinated and effective.</p>
-		</div>
-
- 		<div class="ber">
-		<h2> Amber Willaims: Design Artist </h2>
-			<figure class="firstfig">
-				<img src="images/favphoto.jpg" alt="favphoto">
-				<figcaption>My favorite photo.</figcaption>
-			</figure>
-			<p>My initital role for this assignment is design artist. As the artist I am responsible for everything the user sees, in any browser type and size I decide the graphic design, colors, styles, fonts, layouts for any browser type and size. I will ensure that there are noreadability problems. That the presentation layer (styles) is coded in CSS so it is visually arranged a way that makes sense for the content and adds value to the structure layer. Also that styles (CSS) meet industry standards and best practices including W3C CSS validation within reason.</p>
+			<p> My initital role for this assignment is infromaiton architect. As information architect I must prepare all the content and how it's arranged within each page and across pages. Design the navigation and how it's presented in any browser type and size. Lead team collaboration (Github) and manage the file structure – unless there is a Backend Hacker on the team. I will be ensure that there are no usability problems. That the structure layer (content) is coded in HTML so it is arranged and accessible in a way that makes sense for the content. That content structure(HTML)meets industry standards and best practices including W3C HTML validation within reason and that the team is coordinated and effective.<a href = "mp-about.php"> Read More</a></p>
 		</div>
 
 		<div class="an">
@@ -40,8 +31,17 @@
 				<img src="images/toronto.jpg" alt="Toronto, Canada">
 				<figcaption>Toronto, Canada</figcaption>
 			</figure>
-			<p>My initital role for this assignment is coder. As coder I will be responsible for everything the user interacts with, from page load to every click and scroll. Checks to make sure everything in the front-end (not just JavaScript but also HTML and CSS) must work and work well (optimized). Images must be correctly prepped (optimized). Decides the particulars of plug-in usage and installation, web font installation, and general optimization and usage of all front- end technologies. I will also be in charge of all server interactions using PHP and MySQL including the addition of all code in the HTML documents to handle the server-side interactions (server- side includes too). Responsible for the set up and manipulation of database(s). Leads team collaboration (Github) and manages the file structure.</p>
-        </div>
+			<p>My initital role for this assignment is coder. As coder I will be responsible for everything the user interacts with, from page load to every click and scroll. Checks to make sure everything in the front-end (not just JavaScript but also HTML and CSS) must work and work well (optimized). Images must be correctly prepped (optimized). Decides the particulars of plug-in usage and installation, web font installation, and general optimization and usage of all front- end technologies. I will also be in charge of all server interactions using PHP and MySQL including the addition of all code in the HTML documents to handle the server-side interactions (server- side includes too). Responsible for the set up and manipulation of database(s). Leads team collaboration (Github) and manages the file structure.<a href = "av-about.php"> Read More</a></p>
+		</div>
+
+ 		<div class="ber">
+		<h2> Amber Willaims: Design Artist </h2>
+			<figure class="firstfig">
+				<img src="images/favphoto.jpg" alt="new york">
+				<figcaption>New York City</figcaption>
+			</figure>
+			<p>My initital role for this assignment is design artist. As the artist I am responsible for everything the user sees, in any browser type and size I decide the graphic design, colors, styles, fonts, layouts for any browser type and size. I will ensure that there are noreadability problems. That the presentation layer (styles) is coded in CSS so it is visually arranged a way that makes sense for the content and adds value to the structure layer. Also that styles (CSS) meet industry standards and best practices including W3C CSS validation within reason.<a href = "aw-about.php"> Read More</a></p>
+		</div>
 
 		<?php include "inc/footer.inc"; ?>
 		</div> <!--.container -->
